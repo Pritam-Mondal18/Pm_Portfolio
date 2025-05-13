537288cecaf385f3e1347fade79c236f37945a51
Pritam-Mondal18 <pritammondal18012003@gmail.com>
2025-05-13 14:58:16 +0530

    navbar is now responsived

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const NavbarLogo = () => {
   return (
     <div>
-      <h1 className="text-white text-2xl">Pritam Mondal</h1>
+      <h1 className="text-white text-2xl sm:hidden md:block">Pritam Mondal</h1>
+      <h1 className="text-white font-special font-extrabold text-4xl sm:block md:hidden">
+        𝓟𝓜
+      </h1>
     </div>
   );
 };
