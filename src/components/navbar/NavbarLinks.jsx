95d1a473496dbb143bcdff98bf92d5a6d529bced
Pritam-Mondal18 <pritammondal18012003@gmail.com>
2025-05-13 13:24:17 +0530

    created navbar

--- src/components/navbar/NavbarLinks.jsx
+++ src/components/navbar/NavbarLinks.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
+const links = [
+  { link: "About Me", section: "about" },
+  { link: "Skills", section: "skills" },
+  { link: "Experience", section: "experience" },
+  { link: "Projects", section: "projects" },
+  { link: "Contact", section: "contact" },
+];
+
 const NavbarLinks = () => {
-  return <div></div>;
+  return (
+    <ul className="flex gap-6 text-white font-bold text-center py-4">
+      {links.map((link, index) => {
+        return (
+          <li key={index} className="group">
+            <a
+              href="#"
+              className="cursor-pointer text-white hover:text-cyan transition-all duration 500"
+            >
+              {link.link}
+            </a>
+            <div className="max-auto bg-cyan w-0 group-hover:w-full h-[1px] transition-all duration-500"></div>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default NavbarLinks;
--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const NavbarLogo = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="text-white text-2xl">Pritam Mondal</h1>
+    </div>
+  );
 };
 
 export default NavbarLogo;
